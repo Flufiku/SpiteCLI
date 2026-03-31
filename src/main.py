13afab6962dfc4c78fd72a5d64cbb045afcdceca
Moritz D <moam.dorner@gmail.com>
2026-03-31 02:40:00 +0200

    Adjusted IP

--- src/main.py
+++ src/main.py
@@ -18,7 +18,7 @@ from spite_discord_client import SpiteDiscordClient
 
 
 def main(stdscr):
-    client = SpiteDiscordClient(os.getenv("SPITE_PROXY_URL", "http://127.0.0.1:8000"))
+    client = SpiteDiscordClient(os.getenv("SPITE_PROXY_URL", "http://185.101.159.26:8000"))
     client.run()
     
     state = ""
